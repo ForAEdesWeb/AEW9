7fbcecfb0482513df7dae2d095e5dad8da991b29
Lewis Jang <lewis.aedes@gmail.com>
2016-02-03 13:58:27 +0000

    update joomla version

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,13 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => 'Ih6wIXYYoorhM8uHpvm4qwl0AIN9bqu0',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => '/var/www/knifeking.dsa.tw/tmp/Joomla_3.4.8-Stable-Update_Package.zip',
+	'kickstart.setup.destdir' => '/var/www/knifeking.dsa.tw',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0');
